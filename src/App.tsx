70093ccd204d2b578e3ae6eb1fe35e99721b3677
Codhog <capetechen@gmail.com>
2022-02-24 21:50:49 -0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import { compose, withProps } from "recompose";
+import {MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents} from 'react-leaflet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null)
+
+    const map = useMapEvents({
+        click() {
+            map.locate()
+        },
+        locationfound(e) {
+            // @ts-ignore
+            setPosition(e.latlng)
+            map.flyTo(e.latlng, map.getZoom())
+        },
+    })
+
+    return position === null ? null : (
+        <Marker position={position}>
+            <Popup>You are here</Popup>
+        </Marker>
+    )
 }
 
-export default App;
+function LocationButton(){
+    const getLocation =() =>{
+
+    }
+    return <button onClick={getLocation}></button>
+}
+
+function App() {
+
+    return (
+        <div className="myWrapper">
+            {/*<button onClick={getLocation}>getLocation</button>*/}
+            <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <LocationMarker />
+                <LocationButton />
+            </MapContainer>
+        </div>
+
+    )
+}
+export default App
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styles from 'leaflet/dist/leaflet.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+// import App from "./App";
+import React from "react";
+import App from "./App";
+
+
+const container = document.getElementById('root');
+// Create a root.
+// @ts-ignore
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
